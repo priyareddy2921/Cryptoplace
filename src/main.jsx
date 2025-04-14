64a6a31d9d024b98d0e9e932058a8694c56118e2
priya <priayreddygantla21@gmail.com>
2025-04-14 18:36:16 -0500

    "fixed bug"

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import CoinContextProvider from './context/CoinContext.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Cryptoplace">
       <CoinContextProvider>
          <App />
       </CoinContextProvider>
